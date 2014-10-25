7ebecb5a6f6bce8be0c0bce2fb878ab529aa3404
Sanjay Patel <sanjay@datanautix.com>
2014-10-25 01:48:45 -0400

    cachemartix.R commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function takes a matrix as a parameter.  Matrix is assumed
+## to be a square 
 
-## Write a short comment describing this function
+## It provides a set of functions to interact with the matrix
+##    set - sets the value of the matrix supplied by the calling routine
+##          Also nulls out the inverse of the matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+##    get - returns the value of the matrix
 
+##    setinverse - computes the inverse of the matrix and assignes it to a
+##                 a local object
+
+##    getinverse - returns the inverse of the matrix
+
+makeCacheMatrix <- function(myMatrix = matrix()) {
+  inverse <- NULL
+  set <- function(y) {
+    myMatrix <<- y
+    inverse <<- NULL
+  }
+  get <- function() myMatrix
+  setinverse <- function(solve) inverse <<- solve
+  getinverse <- function() inverse
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## This function returns the inverse of the supplied matrix
+## It first checks to see if the inverse already exists - if it does
+## it returns the inverse otherwise it computes adn then returns the
+## inverse
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(myMatrix, ...) {
+  inverse <- myMatrix$getinverse()
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- myMatrix$get()
+  inverse <- solve(data, ...)
+  myMatrix$setinverse(inverse)
+  inverse
 }
